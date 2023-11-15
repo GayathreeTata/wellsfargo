67c3caf4464384b2c0c88ab3090d1893524a0e4d
GayathreeTata <147612438+GayathreeTata@users.noreply.github.com>
2023-11-15 19:18:27 +0530

    Create Advisor.java

---
+++ Advisor.java
@@ -0,0 +1,102 @@
+package com.wellsfargo.counselor.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.util.ArrayList;
+
+@Entity
+@Table(name="advisors")
+public class Advisor {
+
+    @Id
+    @GeneratedValue()
+    @column(name="id")
+    private long advisorId;
+
+    @OneToMany(mappedBy="advisors")
+    private ArrayList<Client> clients = new ArrayList<Client>();
+
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String email;
+
+    protected Advisor() {
+
+    }
+
+    public Advisor(String firstName, String lastName, String address, String phone, String email, ArrayList<Client> cl) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.clients = cl;
+    }
+
+    public Long getAdvisorId() {
+        return advisorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(ArrayList<Client> clients) {
+        this.clients = clients;
+    }
+}
